2e9be8503f18609be3f1e764e8a8827acf0ff97e
vicpiv <vicpiturca@yahoo.com>
2021-01-06 22:41:26 +0200

    java script 1

---
+++ hello.js
@@ -0,0 +1,23 @@
+var time = 3000
+time = time + 1000
+var message = "Bine ai venit!"
+var isLate = false
+
+if (isLate) {
+    alert("Go home")    
+}
+
+if (isLate) {
+    alert("Go home")
+    } 
+    else {
+    alert("Continue")
+}
+
+setTimeout(welcome, 3000)
+
+function welcome() {
+    alert(message)
+    alert("Welcome!")
+    }
+
