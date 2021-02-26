1048381c8c8a61f3a71f1d890f55ab444ac3f75f
vicpiv <vicpiturca@yahoo.com>
2021-02-26 16:13:37 +0200

    caezar2

--- caesar/caezar.js
+++ caesar/caezar.js
@@ -1,21 +1,35 @@
 document.querySelector("input[type='button']")
-    .addEventListener("click", crypt)
+        .addEventListener("click", crypt)
+
 function crypt() {
     let text = document.querySelector("textarea").value
-    let cypher = document.querySelector("#cypher").value
+    let cypher = Number(document.querySelector("#cypher").value)
 
-    let encryptedText = encrypt(text, cypher)
+    let encryptedText = encryptAscii(text, cypher)
 
     document.querySelector("#result").innerText = encryptedText
-
 }
+
+function encryptAscii(text, cypher) {
+    let encryptedText = text.split("")
+        .map(letter => encryptLetter(letter, cypher))
+        .join("")
+    return encryptedText
+}
+
+function encryptLetter(letter, cypher) {
+    let ascii = letter.charCodeAt(0)
+    let encryptedAscii = (ascii - 65 + cypher) % 26 + 65
+    return String.fromCharCode(encryptedAscii)
+} 
+
 function encrypt(text, cypher) {
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
     let encryptedAlphabet = shift(alphabet, cypher)
     let letterMap = zipAlphabet(alphabet, encryptedAlphabet)
     let encryptedText = text.split("")
-        .map(letter => letterMap[letter])
-        .join("")
+                .map(letter => letterMap[letter])
+                .join("")
     return encryptedText
 }
 
@@ -23,16 +37,14 @@ function shift(alphabet, cypher) {
     let firstCut = alphabet.slice(0, cypher)
     let secondCut = alphabet.slice(cypher)
     return secondCut.concat(firstCut)
-
 }
 
 function zipAlphabet(alphabet1, alphabet2) {
     let zip = {}
-    for (let index; index < alphabet1.lenght; index++) {
+    for (let index = 0; index < alphabet1.length; index++) {
         let firstLetter = alphabet1[index]
         let secondLetter = alphabet2[index]
         zip[firstLetter] = secondLetter
     }
     return zip
-
-}
+} 
